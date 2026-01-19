87fffc2657c1b7a6db1dd96f9eaac4117625bdfe
Sgedelta <Sgeaston04@gmail.com>
2026-01-19 18:02:35 -0500

    bogos game first pass done

--- assets/microgames/BogosBintedGame/BogosBintedGame.cs
+++ assets/microgames/BogosBintedGame/BogosBintedGame.cs
@@ -9,10 +9,20 @@ public partial class BogosBintedGame : MicroBase
     private Sprite2D _bogosUnbinted;
     private Area2D _leftArea;
     private Area2D _rightArea;
+    private AudioStreamPlayer2D _balalaSound;
+    private AudioStreamPlayer2D _bogosSound;
+
 
     private bool _visitedLeftLast = false;
 
     private int _successfulShakes = 0;
+    private int _goalShakes = 0;
+
+    private Timer _gameTimer;
+    private float _goalTime;
+
+    [Export] Godot.Collections.Dictionary<int, float> ShakeDifficultyTargets;
+    [Export] Godot.Collections.Dictionary<int, float> TimeDifficultyTargets;
 
     [Export] Vector2 centerPos;
     [Export] float centerRot;
@@ -24,17 +34,19 @@ public partial class BogosBintedGame : MicroBase
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
         _gm = GetTree().Root.GetNode<GameManager>("GameManager");
         // End unsubscribes these, but anything else you subscribe to these signals NEEDS to be unsubscribed
         _gm.StartGame += Start;
         _gm.InitializeGame += Init;
+
 
         _bogos = GetNode<Node2D>("%Bogos"); //uses godot's Unqiue Name syntax (%)
         _bogosBinted = _bogos.GetChild<Sprite2D>(1);
         _bogosUnbinted = _bogos.GetChild<Sprite2D>(0);
         _leftArea = GetNode<Area2D>("%LeftArea");
         _rightArea = GetNode<Area2D>("%RightArea");
+        _balalaSound = GetNode<AudioStreamPlayer2D>("%Balala");
+        _bogosSound = GetNode<AudioStreamPlayer2D>("%BogosAudio");
 
 
         if (DEBUG_AUTOSTART)
@@ -48,11 +60,20 @@ public partial class BogosBintedGame : MicroBase
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+        if(_gameWon == MicroState.ONGOING)
+        {
+            CalculateProgress();
 
-	}
+        }
+    }
 
     public override void _Input(InputEvent @event)
     {
+        if(_gameWon != MicroState.ONGOING)
+        {
+            return;
+        }
+
         //MnK input is handled in the collision shape callbacks
         if(!_gm.IsMnK && @event is InputEventJoypadMotion)
         {
@@ -82,9 +103,18 @@ public partial class BogosBintedGame : MicroBase
     /// </summary>
     protected override void Init(int difficulty)
     {
+        ReportProgress(1);
+
         _bogosBinted.Modulate = Color.FromHtml("ffffff00");
 
+        _goalShakes = (int)_gm.InterpolateDictionary(ShakeDifficultyTargets, difficulty);
+        _goalTime = _gm.InterpolateDictionary(TimeDifficultyTargets, difficulty);
         
+        if(_gm.DEBUG_MESSAGES)
+        {
+            GD.Print($"[Bogos] Goal Shakes are {_goalShakes} within {_goalTime} seconds");
+        }
+
     }
 
     /// <summary>
@@ -92,10 +122,9 @@ public partial class BogosBintedGame : MicroBase
     /// </summary>
     protected override void Start()
     {
-
         _leftArea.MouseEntered += () =>
         {
-            if (_gm.IsMnK)
+            if (_gameWon == MicroState.ONGOING && _gm.IsMnK)
             {
                 SwapLeft();
             }
@@ -104,7 +133,7 @@ public partial class BogosBintedGame : MicroBase
 
         _leftArea.MouseExited += () =>
         {
-            if (_gm.IsMnK)
+            if (_gameWon == MicroState.ONGOING && _gm.IsMnK)
             {
                 SwapCenter();
             }
@@ -112,7 +141,7 @@ public partial class BogosBintedGame : MicroBase
 
         _rightArea.MouseEntered += () =>
         {
-            if (_gm.IsMnK)
+            if (_gameWon == MicroState.ONGOING && _gm.IsMnK)
             {
                 SwapRight();
             }
@@ -120,12 +149,36 @@ public partial class BogosBintedGame : MicroBase
 
         _rightArea.MouseExited += () =>
         {
-            if (_gm.IsMnK)
+            if (_gameWon == MicroState.ONGOING && _gm.IsMnK)
             {
                 SwapCenter();
             }
         };
 
+
+        _gameTimer = new Timer();
+        _gameTimer.WaitTime = _goalTime;
+        _gameTimer.OneShot = true;
+
+        _gameTimer.Timeout += () =>
+        {
+            if (_gameWon != MicroState.WON)
+            {
+                _gameWon = MicroState.LOST;
+            }
+            _gameStarted = false; //honestly, I would maybe move this to tracking the _gameWon, and preventing all input if _gameWon is not ONGOING, but I want to get the example done lol
+            _gameTimer.QueueFree();
+
+            End();
+
+        };
+
+        AddChild(_gameTimer);
+        _gameTimer.Start();
+
+
+        _gameStarted = true;
+        _gameWon = MicroState.ONGOING;
     }
 
     /// <summary>
@@ -133,7 +186,58 @@ public partial class BogosBintedGame : MicroBase
     /// </summary>
     protected override void CalculateProgress()
     {
+        float progress = 1;
 
+        if (_gameTimer != null)
+        {
+            progress = (float)_gameTimer.TimeLeft / _goalTime;
+        }
+
+
+        ReportProgress(progress);
+    }
+
+    private void CompletedShake()
+    {
+        _successfulShakes++;
+        if (DEBUG_MESSAGES)
+        {
+            GD.Print("[Bogos] Completed Shake: " + _successfulShakes);
+        }
+
+        float shakePercent = Mathf.Clamp((float)_successfulShakes / _goalShakes, 0, 1);
+
+        _bogosBinted.Modulate = new Color(1, 1, 1, shakePercent);
+        _bogosUnbinted.Modulate = new Color(1, 1, 1, 1.0f - shakePercent);
+        _balalaSound.Play();
+
+
+        if (_successfulShakes >= _goalShakes)
+        {
+            WinAnim();
+        }
+    }
+
+    private void WinAnim()
+    {
+        _gameWon = MicroState.WON;
+        SwapCenter();
+        _gameTimer.Stop();
+
+        Tween winTween = GetTree().CreateTween();
+
+        //Modulate to smiling here
+        //winTween.TweenProperty();
+        //winTween.Parallel().TargetProperty();
+
+        winTween.TweenCallback(Callable.From(() => {
+            _bogosSound.Play();
+        
+        }));
+
+        winTween.TweenCallback(Callable.From(() => { End(); })).SetDelay(1);
+
+        
     }
 
 
@@ -147,7 +251,7 @@ public partial class BogosBintedGame : MicroBase
         //check if it's worth a shake credit
         if (!_visitedLeftLast)
         {
-            _successfulShakes++;
+            CompletedShake();
         }
 
         //update last visited
@@ -164,7 +268,7 @@ public partial class BogosBintedGame : MicroBase
         //check if it's worth a shake credit
         if (_visitedLeftLast)
         {
-            _successfulShakes++;
+            CompletedShake();
         }
 
         //update last visited
@@ -176,4 +280,6 @@ public partial class BogosBintedGame : MicroBase
         _bogos.Position = centerPos;
         _bogos.RotationDegrees = centerRot;
     }
+
+
 }
